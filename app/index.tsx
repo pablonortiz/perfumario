6d059d22f213aeaa39cdec0040bd13cf72484abb
Pablo Ortiz <pablonortiz05@hotmail.com>
2025-09-17 21:27:26 -0300

    Initial commit

---
+++ app/index.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Los cambios son sobre esta screen</Text>
+    </View>
+  );
+}
